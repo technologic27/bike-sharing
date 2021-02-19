6c67a6fdbab44b794168d26ec25114a596649218
Esther Dawes <esther.dawes@bbp.sg>
2021-02-19 12:08:10 +0800

    new function for confidence interval

--- models/model_run.py
+++ models/model_run.py
@@ -2,14 +2,40 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, 
 from sklearn.linear_model import Ridge
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from models.model_train import TrainPredictDuration
-import numpy
+import stats
+import numpy as np
+
+
+def get_prediction_interval(prediction, y_test, test_predictions, pi=.95):
+    """
+    Get a prediction interval for a linear regression.
+
+    INPUTS:
+        - Single prediction,
+        - y_test
+        - All test set predictions,
+        - Prediction interval threshold (default = .95)
+    OUTPUT:
+        - Prediction interval for single prediction
+    """
+    # get standard deviation of y_test
+    sum_errs = np.sum((y_test - test_predictions) ** 2)
+    stdev = np.sqrt(1 / (len(y_test) - 2) * sum_errs)
+    # get interval from standard deviation
+    one_minus_pi = 1 - pi
+    ppf_lookup = 1 - (one_minus_pi / 2)
+    z_score = stats.norm.ppf(ppf_lookup)
+    interval = z_score * stdev
+    # generate prediction interval lower and upper bound
+    lower, upper = prediction - interval, prediction + interval
+    return lower, prediction, upper
 
 
 def main():
 
-    estimators = [('rf', RandomForestRegressor())]
+    estimators = [('lr', Ridge())]
 
-    backup = [('gb', GradientBoostingRegressor()),('lr', Ridge())]
+    backup = [('gb', GradientBoostingRegressor()),('rf', RandomForestRegressor())]
 
     run = TrainPredictDuration('resource/config.ini', 'model')
     subscr_type = run.subscr_type
